5a7b6d9a53afadd5f119bb01c1ce3aa9fc23cf01
Guillermo Bascuñana <stygmah@gmail.com>
2023-02-14 23:34:46 +0100

    cosmetic

--- src/components/NavBar/styled.js
+++ src/components/NavBar/styled.js
@@ -13,6 +13,7 @@ export const Container = styled.div`
 export const UserInfo = styled.button`
   background-color: var(--white);
   border: none;
+  cursor: pointer;
 
 `;
 export const UserInfoMenu = styled.div`
